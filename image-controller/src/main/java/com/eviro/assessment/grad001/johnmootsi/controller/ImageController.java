3b7a8eeacaabdee589798b21e7f0e54d85c03b21
Johnny Mootsi <johnnymootsi@pop-os.localdomain>
2023-06-09 02:57:13 +0200

    fixing the image controller class

--- image-controller/src/main/java/com/eviro/assessment/grad001/johnmootsi/controller/ImageController.java
+++ image-controller/src/main/java/com/eviro/assessment/grad001/johnmootsi/controller/ImageController.java
@@ -9,7 +9,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/api/image")
 public class ImageController {
-    @GetMapping(value = "/{name}/{surname}/{\\w\\.\\w}")
+
+    @GetMapping("/test")
+    public String hello() {
+        return "Testing Hello";
+    }
+
+    @GetMapping(value = "/{name}/{surname}/{\\w\\.\\w")
     public FileSystemResource gethttpImageLink(@PathVariable String name,@PathVariable String surname) {
         // Retrieve the image file path based on the name and surname
         AccountProfileService accountProfileService = new AccountProfileService();
