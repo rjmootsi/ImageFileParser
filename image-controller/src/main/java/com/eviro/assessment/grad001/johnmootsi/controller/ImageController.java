35fe7eb70f244b7ffe003f4d9e3b7ddece211459
Johnny Mootsi <johnnymootsi@pop-os.localdomain>
2023-06-09 03:51:40 +0200

    removed the test endpoint

--- image-controller/src/main/java/com/eviro/assessment/grad001/johnmootsi/controller/ImageController.java
+++ image-controller/src/main/java/com/eviro/assessment/grad001/johnmootsi/controller/ImageController.java
@@ -9,12 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/api/image")
 public class ImageController {
-
-    @GetMapping("/test")
-    public String hello() {
-        return "Testing Hello";
-    }
-
+    
     @GetMapping(value = "/{name}/{surname}/{\\w\\.\\w")
     public FileSystemResource gethttpImageLink(@PathVariable String name,@PathVariable String surname) {
         // Retrieve the image file path based on the name and surname
